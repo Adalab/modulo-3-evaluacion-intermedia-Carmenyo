b68e91ec10ee16d03cca75a4a5e27a2430f2ba12
Carmenyo <carmencastroyo@gmail.com>
2022-04-26 08:55:08 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import '../styles/App.scss';
+import phrases from "../data/phrases.json"
+console.log(phrases)
+
+function App() {
+
+
+  return (
+
+    <>
+      <header className='header'>
+        <h1 className='header__title'>Frases Célebres Friends</h1>
+      </header>
+
+
+    </>
+  );
+}
+
+export default App;
